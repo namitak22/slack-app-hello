39791aa2fdfc1ea0e6247c5e768650a70998c488
namitak22 <32341160+namitak22@users.noreply.github.com>
2018-07-29 21:39:43 -0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World Namita!"
+    return "Hello, World Namita !"
 
 @app.route('/hello', methods=['POST'])
 def hello():
